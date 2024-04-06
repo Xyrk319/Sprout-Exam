419026f3c5c78f034d6abf276dafa2fd00158bf3
Jonahlee Xyrkcysz Fabula <xyrk.fabula@dice205.com>
2024-04-06 21:05:07 +0800

    feat: init authstore and axios helpers

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,6 @@ import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import vueJsx from '@vitejs/plugin-vue-jsx'
 import VueDevTools from 'vite-plugin-vue-devtools'
-
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
@@ -14,7 +13,13 @@ export default defineConfig({
   ],
   resolve: {
     alias: {
-      '@': fileURLToPath(new URL('./src', import.meta.url))
+      '@': fileURLToPath(new URL('./src', import.meta.url)),
+      '@helpers': fileURLToPath(new URL('./src/helpers', import.meta.url)),
+      '@components': fileURLToPath(new URL('./src/components', import.meta.url)),
+      '@stores': fileURLToPath(new URL('./src/stores', import.meta.url)),
+      '@views': fileURLToPath(new URL('./src/views', import.meta.url)),
+      '@router': fileURLToPath(new URL('./src/router', import.meta.url)),
+      '@interfaces': fileURLToPath(new URL('./src/interfaces', import.meta.url))
     }
   }
 })
